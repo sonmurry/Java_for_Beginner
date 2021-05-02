34ab87774eac9b077bad25277a8067d560052884
Son Murry <66saenggak99@gmail.com>
2021-05-02 21:13:26 +0900

    예제

--- src/Ex06_test.java
+++ src/Ex06_test.java
@@ -1,9 +1,11 @@
+import java.util.Scanner;
 
 public class Ex06_test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner s = new Scanner (System.in);
+				
 	}
 
 }
