d4253bfea57abb2919c90e97152e4ac09d5194db
Son Murry <66saenggak99@gmail.com>
2021-05-04 18:29:42 +0900

    06_for문_10부터 20까지 표시

---
+++ src/Ex06_203p_Directly_03.java
@@ -0,0 +1,13 @@
+
+public class Ex06_203p_Directly_03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		
+		for (i = 10; i <=20; i++) {
+			System.out.printf("%d \n", i);
+		}
+	}
+
+}
