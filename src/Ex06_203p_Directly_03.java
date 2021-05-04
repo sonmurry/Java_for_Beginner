094852bfff9a76da32458d372e066e5d38aaee08
Son Murry <66saenggak99@gmail.com>
2021-05-04 18:27:01 +0900

    06_for문_for문을 쓰지않고 1부터 10까지의 합을 나타내기

---
+++ src/Ex06_203p_Directly_03.java
@@ -0,0 +1,15 @@
+
+public class Ex06_203p_Directly_03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int hap;
+		
+		hap = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10;
+		
+		System.out.printf(" 1에서 10까지의 합: %d \n", hap);
+		
+		
+	}
+
+}
