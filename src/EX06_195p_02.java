98ceea10333479f4eb92693871c2c59414ceb9e3
Son Murry <66saenggak99@gmail.com>
2021-05-04 17:44:12 +0900

    06_for문_02_for문을 사용한 단순예시(5회반복)

---
+++ src/EX06_195p_02.java
@@ -0,0 +1,14 @@
+
+public class EX06_195p_02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		
+		for (i = 0; i < 5; i++) {
+			System.out.printf("안녕하세요? 빙글빙글 for 문을 공부중입니다.^^\n");
+			
+		}
+	}
+
+}
