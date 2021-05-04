67d2a6c5e792c9fd167021822d70d37656e41acd
Son Murry <66saenggak99@gmail.com>
2021-05-05 01:22:29 +0900

    06_for문_1~12345까지의 합

---
+++ src/Ex06_205p_Directly_04.java
@@ -0,0 +1,19 @@
+
+public class Ex06_205p_Directly_04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i, hap = 0;
+		
+		
+		
+		for (i = 1; i <= 12345; i++) {
+			hap += i;
+		}
+		
+		System.out.printf(" 1에서 10까지의 합: %d \n", hap);
+		
+		
+	}
+
+}
