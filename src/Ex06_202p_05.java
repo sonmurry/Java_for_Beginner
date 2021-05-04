bdfd5919022d17a55beac3219459c6a2f861f43a
Son Murry <66saenggak99@gmail.com>
2021-05-04 18:23:04 +0900

    06_for문_조건식의 i값을 출력할 경우

---
+++ src/Ex06_202p_05.java
@@ -0,0 +1,14 @@
+
+public class Ex06_202p_05 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		
+		for (i = 1; i <=5; i++) {
+			System.out.printf("%d \n", i);
+			
+		}
+	}
+
+}
