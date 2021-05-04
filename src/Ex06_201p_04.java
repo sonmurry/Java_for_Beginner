4e50a545da21fa5c2ed6ddf6f20f52c06b0d3ef2
Son Murry <66saenggak99@gmail.com>
2021-05-04 18:20:25 +0900

    06_for문_증감식의 변경

---
+++ src/Ex06_201p_04.java
@@ -0,0 +1,14 @@
+
+public class Ex06_201p_04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		
+		for (i = 5; 0 < i; i--) {
+			System.out.printf("안녕하세요? 빙글빙글 for 문을 공부중입니다.^^\n");
+			
+		}
+	}
+
+}
