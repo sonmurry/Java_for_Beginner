686b12aad34350e6a9e0351589a69b3a9579793a
Son Murry <66saenggak99@gmail.com>
2021-05-04 18:03:17 +0900

    06_for문_중괄호 사용 예

---
+++ src/Ex06_200p_04.java
@@ -0,0 +1,14 @@
+
+public class Ex06_200p_04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		
+		for (i = 0; i < 5; i++) {
+			System.out.printf("안녕하세요? 빙글빙글 for 문을 공부중입니다.^^\n");
+			
+		}
+	}
+
+}
