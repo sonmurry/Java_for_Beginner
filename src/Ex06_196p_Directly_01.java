1b1d84a35f4fcf9b5d63327f5c1b327bb044cd97
Son Murry <66saenggak99@gmail.com>
2021-05-04 17:50:18 +0900

    06_for문_줄바꿈X

---
+++ src/Ex06_196p_Directly_01.java
@@ -0,0 +1,13 @@
+
+public class Ex06_196p_Directly_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		
+		for (i = 0; i <20; i++) {
+			System.out.print("안녕하세요? 빙글빙글 for 문을 공부중입니다. ^^ ");
+		}
+	}
+
+}
