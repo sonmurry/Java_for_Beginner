de61e82735efa0d443b14b8f17308fadbaeead3d
Son Murry <66saenggak99@gmail.com>
2021-05-04 20:11:25 +0900

    06_for문_for문을 이용한 합(값초기화의 중요성)

---
+++ src/Ex06_204p_07.java
@@ -0,0 +1,19 @@
+
+public class Ex06_204p_07 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i, hap;
+		
+		
+		
+		for (i = 1; i <= 10; i++) {
+			hap = hap + i;
+		}
+		
+		System.out.printf(" 1에서 10까지의 합: %d \n", hap);
+		
+		
+	}
+
+}
