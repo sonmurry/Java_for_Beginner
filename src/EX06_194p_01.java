2593f990a4eef83dd00bb623c27cfa3e33e2cdb9
Son Murry <66saenggak99@gmail.com>
2021-05-04 17:40:36 +0900

    6.for문 예제01_단순복사

---
+++ src/EX06_194p_01.java
@@ -0,0 +1,13 @@
+
+public class EX06_194p_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.printf("안녕하세요 빙글빙글 for 문을 공부중입니다. ^^\n");
+		System.out.printf("안녕하세요 빙글빙글 for 문을 공부중입니다. ^^\n");
+		System.out.printf("안녕하세요 빙글빙글 for 문을 공부중입니다. ^^\n");
+		System.out.printf("안녕하세요 빙글빙글 for 문을 공부중입니다. ^^\n");
+		System.out.printf("안녕하세요 빙글빙글 for 문을 공부중입니다. ^^\n");
+	}
+
+}
