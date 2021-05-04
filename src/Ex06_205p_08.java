f16c3a2c1398b10071a153d4ee2f47e9d68b4cbe
Son Murry <66saenggak99@gmail.com>
2021-05-04 20:14:04 +0900

    06_for문_'+='을 이용한 합 자동계산

---
+++ src/Ex06_205p_08.java
@@ -0,0 +1,19 @@
+
+public class Ex06_205p_08 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i, hap = 0;
+		
+		
+		
+		for (i = 1; i <= 10; i++) {
+			hap += i;
+		}
+		
+		System.out.printf(" 1에서 10까지의 합: %d \n", hap);
+		
+		
+	}
+
+}
