fcabf4fc625397519d630123998afd42be4b957e
Son Murry <66saenggak99@gmail.com>
2021-05-04 17:58:22 +0900

    06_for문_초깃값의 변화

---
+++ src/Ex06_199p_Directly_02.java
@@ -0,0 +1,13 @@
+
+public class Ex06_199p_Directly_02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		
+		for (i = 1; i <4; i++) {
+			System.out.print("안녕하세요? 빙글빙글 for 문을 공부중입니다. ^^ \n");
+		}
+	}
+
+}
