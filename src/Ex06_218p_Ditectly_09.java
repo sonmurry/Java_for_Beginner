3f284aef4b93ccef7951bd482b86fff04d9ccf0a
Son Murry <66saenggak99@gmail.com>
2021-05-07 17:28:58 +0900

    06_for문_중첩for문을 활용한 구구단_가로출력_거꾸로

---
+++ src/Ex06_218p_Ditectly_09.java
@@ -0,0 +1,17 @@
+
+public class Ex06_218p_Ditectly_09 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub	
+		int i, j;
+		
+		for (j = 9; j >=1; j--) {
+			for ( i= 9; i >=1; i--) {
+				System.out.printf(" %d X %d = %2d    ", i, j, i*j);
+			}
+			System.out.printf("\n");
+		}
+		
+	}
+
+}
