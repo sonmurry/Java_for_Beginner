efc7098f318ada3458bd1659a686693d3518fc9b
Son Murry <66saenggak99@gmail.com>
2021-05-07 17:07:47 +0900

    06_for문_중첩 for문 예시 (i, j를 이용하여)

---
+++ src/Ex06_212p_13.java
@@ -0,0 +1,16 @@
+
+public class Ex06_212p_13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i, j;
+		
+		for (i = 0; i < 3; i++) {
+			
+			for (j = 0; j < 2; j++) {
+				System.out.printf("중첩 for 문입니다. (i값: %d, j값: %d)\n", i, j);
+			}
+		}
+	}
+
+}
