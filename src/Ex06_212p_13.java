67076044b110724bf303840423302e79efa286c1
Son Murry <66saenggak99@gmail.com>
2021-05-07 17:09:46 +0900

    06_for문_중첩 for문 예시 (i, j)

--- src/Ex06_212p_13.java
+++ src/Ex06_212p_13.java
@@ -5,12 +5,10 @@ public class Ex06_212p_13 {
 		// TODO Auto-generated method stub
 		int i, j;
 		
-		for (i = 0; i < 3; i++) {
-			
+		for (i = 0; i < 3; i++) 
 			for (j = 0; j < 2; j++) {
 				System.out.printf("중첩 for 문입니다. (i값: %d, j값: %d)\n", i, j);
 			}
-		}
 	}
 
 }
