c1980ea5eeaba62bf5912308db623fc2fa0e6f48
Son Murry <66saenggak99@gmail.com>
2021-05-08 00:12:14 +0900

    06_for문_기타 for문_초깃값2개, 증감식2개

---
+++ src/Ex06_219p_16.java
@@ -0,0 +1,13 @@
+
+public class Ex06_219p_16 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i, k;
+		
+		for (i = 1, k = 1; i <= 9; i++, k++) {
+			System.out.printf("%d X %d = %d \n", i, k, i*k);
+		}
+	}
+
+}
