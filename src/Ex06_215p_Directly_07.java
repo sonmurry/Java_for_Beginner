017f1f5d3c4d5afe4bb11db3221ba3ade4d3e5ae
Son Murry <66saenggak99@gmail.com>
2021-05-07 17:12:22 +0900

    06_for문_i(2~9까지), j(1~9까지)출력

---
+++ src/Ex06_215p_Directly_07.java
@@ -0,0 +1,14 @@
+
+public class Ex06_215p_Directly_07 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i, j;
+		
+		for (i = 2; i <= 9; i++) 
+			for (j = 1; j <= 9; j++) {
+				System.out.printf("중첩 for 문입니다. (i값: %d, j값: %d)\n", i, j);
+			}
+	}
+
+}
