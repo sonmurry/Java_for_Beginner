a9e0258e99d06ed30cedb823bd5b104d1f5127c6
Son Murry <66saenggak99@gmail.com>
2021-05-07 17:27:42 +0900

    06_for문_중첩for문을 활용한 구구단_가로출력

---
+++ src/Ex06_217p_15.java
@@ -0,0 +1,17 @@
+
+public class Ex06_217p_15 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub	
+		int i, j;
+		
+		for (j = 1; j <=9; j++) {
+			for ( i= 1; i <=9; i++) {
+				System.out.printf(" %d X %d = %2d    ", i, j, i*j);
+			}
+			System.out.printf("\n");
+		}
+		
+	}
+
+}
