13901232f5dede8a2acfe01ebed2d421f4e86116
Son Murry <66saenggak99@gmail.com>
2021-05-07 17:16:39 +0900

    06_for문_중첩 for을 이용한 구구단(단별로 띄어쓰기)

---
+++ src/Ex06_215p_14.java
@@ -0,0 +1,17 @@
+
+public class Ex06_215p_14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i, j;
+		
+		for (i = 2; i <= 9; i++) {
+			for (j = 1; j <= 9; j++) {
+				System.out.printf("%d X %d = %d\n", i, j, i*j);
+			}
+			System.out.print("\n");	
+		}
+
+	}
+
+}
