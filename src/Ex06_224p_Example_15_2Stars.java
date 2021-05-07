1b76914b3cf2781eccc0fe399b12bcc832013ca1
Son Murry <66saenggak99@gmail.com>
2021-05-08 01:05:47 +0900

    06_for문_예제15_아스키코드표 출력

--- src/Ex06_224p_Example_15_2Stars.java
+++ src/Ex06_224p_Example_15_2Stars.java
@@ -4,11 +4,14 @@ public class Ex06_224p_Example_15_2Stars {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int i;
-		System.out.println("------------------------");
-		System.out.println("10진수\t16진수\t문자");
-		System.out.println("------------------------");
+		
 		
 		for (i = 0; i <= 127; i++) {
+			if (i%16 == 0) {
+				System.out.println("------------------------");
+				System.out.println("10진수\t16진수\t문자");
+				System.out.println("------------------------");
+			}
 			System.out.printf("%6d %6x %6c\n",i,i,i);
 		}
 	}
