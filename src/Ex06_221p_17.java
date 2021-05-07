6c358a11adca3e3955a3e6bc3266a2990a94f25a
Son Murry <66saenggak99@gmail.com>
2021-05-08 00:26:18 +0900

    06_for문_무한루프(조건식 누락)일부러 사용하는 경우도 있습니다.

---
+++ src/Ex06_221p_17.java
@@ -0,0 +1,14 @@
+
+public class Ex06_221p_17 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		i = 0;
+		for (; ;) {
+			System.out.printf("%d \n", i);
+			i++;
+		}
+	}
+
+}
