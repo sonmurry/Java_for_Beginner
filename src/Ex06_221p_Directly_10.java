5c1153ef88a0ad297fbfb07aa01b599d02cf7d6d
Son Murry <66saenggak99@gmail.com>
2021-05-08 00:22:08 +0900

    06_for문_for문에서 초깃값과 증감식을 빼내어 1~100까지 합 출력

---
+++ src/Ex06_221p_Directly_10.java
@@ -0,0 +1,17 @@
+
+public class Ex06_221p_Directly_10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 1, hap = 0;
+		
+		for (; i <= 100;) {
+			
+			hap += i;
+			i++;
+		}
+		System.out.printf(" 1부터 100까지의 합: %d", hap);
+		
+	}
+
+}
