4feec15fdbcba4d37cdaaccff4efcd48697bbece
Son Murry <66saenggak99@gmail.com>
2021-05-08 23:30:10 +0900

    07_while문_for문(Ex06_2)을 while문으로 바꾸어 봅시다.

---
+++ src/Ex07_235p_01.java
@@ -0,0 +1,15 @@
+
+public class Ex07_235p_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		i = 0;
+		
+		while (i < 5) {
+			System.out.printf("while 문을 공부합니다.\n");
+			i++;
+		}
+	}
+
+}
