b08d3533c86b55e1e275251f80faa968fee8a09d
Son Murry <66saenggak99@gmail.com>
2021-05-08 23:32:42 +0900

    07_while문_for문(Ex06_8)을 while문으로 바꾸기

---
+++ src/Ex07_236p_02.java
@@ -0,0 +1,18 @@
+
+public class Ex07_236p_02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int hap = 0;
+		int i;
+		
+		i = 0;
+		while ( i <= 10) {
+			hap += i;
+			i++;
+		}
+		System.out.printf(" 1에서 10까지의 합 : %d \n", hap);
+		
+	}
+
+}
