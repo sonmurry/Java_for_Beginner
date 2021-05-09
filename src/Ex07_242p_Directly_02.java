1133ef1bb247e6a3858c13a483b30c7ccacf59fe
Son Murry <66saenggak99@gmail.com>
2021-05-09 15:00:57 +0900

    07_while문_do ~ while문으로 변환

---
+++ src/Ex07_242p_Directly_02.java
@@ -0,0 +1,18 @@
+
+public class Ex07_242p_Directly_02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int hap = 0;
+		int i;
+		
+		i = 0;
+		
+		do {
+			hap += i;
+			i++;
+		}while ( i <= 10); 
+		System.out.printf(" 1에서 10까지의 합 : %d \n", hap);
+	}
+
+}
