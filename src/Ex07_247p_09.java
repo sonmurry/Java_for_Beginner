9b6c72dcd8faf848552cf389821608a2e8f652d3
Son Murry <66saenggak99@gmail.com>
2021-05-09 15:53:58 +0900

    07_while문_1부터 100까지의 숫자의 합중 총합이 1000을 넘기는 시점(실수주의)

--- src/Ex07_247p_09.java
+++ src/Ex07_247p_09.java
@@ -5,7 +5,7 @@ public class Ex07_247p_09 {
 		// TODO Auto-generated method stub
 		int i = 0, hap = 0;
 		
-		for (;i <= 100; i++) {
+		for (;i <= 100; i++) { 		//만약 while문을 통해 증감식을 수행하도록 하면 45가 아닌 46이 값으로 나온다. 주의할 것.
 			hap += i;
 			
 			
