f558578174b1f4a984844de6748a04cdadcf0845
Son Murry <66saenggak99@gmail.com>
2021-05-09 14:54:32 +0900

    07_while문_do ~ while문의 차이

---
+++ src/Ex07_241p_05.java
@@ -0,0 +1,16 @@
+
+public class Ex07_241p_05 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 100;
+		
+		while ( a == 200) {
+			System.out.printf("while 문 내부에 들어 왔습니다. \n");
+		}
+		do {
+			System.out.printf("do ~ while 문 내부에 들어 왔습니다.\n");
+		}while (a == 200);
+	}
+
+}
