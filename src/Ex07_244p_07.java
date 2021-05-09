f620352e4b0ddd1d3166eae0904fe4bc37d9989b
Son Murry <66saenggak99@gmail.com>
2021-05-09 15:33:17 +0900

    07_while문_기타제어문 1)'break'

---
+++ src/Ex07_244p_07.java
@@ -0,0 +1,16 @@
+
+public class Ex07_244p_07 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		
+		for (i = 1; i <= 100; i++) {
+			System.out.printf("for 문을 %d 회 실행했습니다.\n", i);
+			break;
+		}
+		
+		System.out.printf("for 문을 종료했습니다.\n");
+	}
+
+}
