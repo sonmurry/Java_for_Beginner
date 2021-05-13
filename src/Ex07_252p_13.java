07e0a72f0cdc4c04be2beeabb6291e13c0cfae31
Son Murry <66saenggak99@gmail.com>
2021-05-14 01:19:12 +0900

    07_while문_return문의 실습. 하지만 무한반복 확인용 메소드가 오류가 난다.

---
+++ src/Ex07_252p_13.java
@@ -0,0 +1,19 @@
+
+public class Ex07_252p_13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int hap = 0;
+		int i;
+		
+		for (i = 1; i <=100; i++) 
+			hap += i;
+			
+			System.out.printf("1부터 100까지의 합은 %d 입니다.\n",  hap);
+			return;
+			
+			
+		
+	}
+
+}
