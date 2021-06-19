6b681ad7124b9c8dcb266e9ef5ee78ef3b436d96
Yi <66saenggak99@gmail.com>
2021-06-19 23:55:44 +0900

    09_문자열_length() 메소드 사용 예 1

---
+++ src/Ex09_302p_01.java
@@ -0,0 +1,16 @@
+
+public class Ex09_302p_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "IT CookBook.Java";
+		int len;
+		
+		len = str.length();
+		
+		System.out.printf("문자열 : %s \n", str);
+		System.out.printf("문자열 길이 : %d ", len);
+		
+	}
+
+}
