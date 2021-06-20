fb05673efc57a0e6a9719172aded6d2fd64b3b95
Yi <66saenggak99@gmail.com>
2021-06-21 07:54:02 +0900

    09_문자열_두 문자열 비교, 문자열 포함하는 지 확인하는 메소드

---
+++ src/Ex09_309p_08.java
@@ -0,0 +1,17 @@
+
+public class Ex09_309p_08 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str1 = "Java Programming";
+		String str2 = "Java IT CookBook";
+		
+		System.out.printf("원 문자열1 ==> [%s]\n", str1);
+		System.out.printf("원 문자열2 ==> [%s]\n", str2);
+		
+		System.out.printf("%s\n", str1.compareTo(str2));
+		System.out.printf("%s\n", str1.contains("java"));
+
+	}
+
+}
