c26aea7d71fde9ed229d1433aa07b8db78dd9056
Yi <66saenggak99@gmail.com>
2021-06-20 17:48:57 +0900

    09_문자열_대문자로, 소문자로, 공백제거(중간공백은 불가)

---
+++ src/Ex09_308p_06.java
@@ -0,0 +1,14 @@
+
+public class Ex09_308p_06 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "	한글	ABCD	efgh	";
+		
+		System.out.println("원 문자열 ==> [" + str + "]");
+		System.out.println("대문자로 ==> [" + str.toUpperCase() + "]");
+		System.out.println("소문자로 ==> [" + str.toLowerCase() + "]");
+		System.out.println("공백제거 ==> [" + str.trim() + "]");
+	}
+
+}
