b192bd345d003b77b481ba0b02cea45a7f2497c3
Yi <66saenggak99@gmail.com>
2021-06-21 09:12:54 +0900

    09_메소드_합계 계산 메소드 제작

---
+++ src/Ex09_321p_13.java
@@ -0,0 +1,19 @@
+
+public class Ex09_321p_13 {
+
+	
+	static int plus(int v1, int v2) {
+		int result;
+		result = v1 + v2;
+		return result;
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int hap;
+		hap = plus(100, 200);
+		System.out.printf("100과 200의 plus() 메소드 결과는 : %d\n", hap);
+	}
+
+}
