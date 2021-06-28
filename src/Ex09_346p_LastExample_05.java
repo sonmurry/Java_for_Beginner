b1e691343c95b6018e1b45c0cfe55532e93a37a7
Yi <66saenggak99@gmail.com>
2021-06-28 09:28:10 +0900

    09_메소드_전역변수와 지역변수간의 우선순위

---
+++ src/Ex09_346p_LastExample_05.java
@@ -0,0 +1,13 @@
+
+public class Ex09_346p_LastExample_05 {
+
+	int a = 100;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 200;
+		
+		System.out.printf("%d", a);
+	}
+
+}
