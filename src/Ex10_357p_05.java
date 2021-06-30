afd7e82b3bf68ba266df5dee0998db69d3cc21bd
Yi <66saenggak99@gmail.com>
2021-06-30 14:08:51 +0900

    10_표준입출력_서식화된 출력 메소드

---
+++ src/Ex10_357p_05.java
@@ -0,0 +1,24 @@
+
+public class Ex10_357p_05 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 1234;
+		float b = 100.12345f;
+		char ch = 'k';
+		String s = "IT_Cookbook";
+		
+		System.out.printf("정수의 10진수 ==> %d\n", a);
+		System.out.printf("정수의 16진수 ==> %x\n", a);
+		System.out.printf("정수의 8진수 ==> %o\n", a);
+		System.out.printf("실수 ==> %10.3f\n", b);
+		System.out.printf("실수(공학용) ==> %e\n", b);
+		System.out.printf("문자 ==> %c\n", ch);
+		System.out.printf("문자열 ==> %s\n", s);
+		
+		
+		
+	}
+	
+
+}
