da96a77457808edea2ec1efb4aa1473281e385b3
Yi <66saenggak99@gmail.com>
2021-07-09 16:08:55 +0900

    10_표준입출력_표준입력 사용 예

---
+++ src/Ex10_358p_06.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Ex10_358p_06 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		byte a;
+		short b;
+		int c;
+		long d;
+		float e;
+		double f;
+		String str1, str2;
+		
+		System.out.print("byte : "); a = s.nextByte();
+		System.out.print("short : "); b = s.nextShort();
+		System.out.print("Int : "); c = s.nextInt();
+		System.out.print("Long : "); d = s.nextLong();
+		System.out.print("float : "); e = s.nextFloat();
+		System.out.print("Double : "); f = s.nextDouble();
+		System.out.print("str1 : "); str1 = s.next();
+		System.out.print("str2 : "); str2 = s.nextLine();
+		
+	}
+
+}
