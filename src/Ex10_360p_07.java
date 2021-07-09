243a2cef38768c880060dc7a67222d657ae0f0b0
Yi <66saenggak99@gmail.com>
2021-07-09 16:15:44 +0900

    10_파일입출력_next()의 작동 예

---
+++ src/Ex10_360p_07.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Ex10_360p_07 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		
+		String str1, str2, str3;
+		
+		System.out.print("단어 3개 입력  :  ");
+		str1 = s.next();
+		str2 = s.next();
+		str3 = s.next();
+		
+		System.out.print("입력된 문자열 ==> ");
+		System.out.print(str1 +"," +str2 +"," +str3);
+		
+	}
+
+}
