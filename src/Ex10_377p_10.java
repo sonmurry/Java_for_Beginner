7eedb17d25ccccc03d12edbf166a410b7c165d71
Son Murry <66saenggak99@gmail.com>
2021-07-15 16:56:25 +0900

    10_파일입출력_파일을 이용한 입력2

---
+++ src/Ex10_377p_10.java
@@ -0,0 +1,21 @@
+import java.io.FileInputStream;
+
+public class Ex10_377p_10 {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		FileInputStream fis = new FileInputStream("c:/temp/data1.txt");
+		int ch;
+		byte[] bt = new byte[1024];
+		
+		int i = 0;
+		while ((ch = fis.read()) != -1) {
+			bt[i] = (byte)ch;
+			i++;
+		}
+		
+		System.out.print(new String(bt)); //byte형식의 배열을 String형식으로 출력
+		fis.close();
+	}
+
+}
